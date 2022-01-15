4f1dad09cdacaf7843d243b46cab44f7e9fd22fc
Jack <jack.branthwaite@gmail.com>
2022-01-15 23:29:55 +1300

    Footer progress - missing socials and svgs

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -15,7 +15,27 @@
       </ul>
       <div class="living-wage-container"></div>
     </div>
-    <div class="opening-container"></div>
+    <div class="opening-container">
+      <div class="opening-one">
+        <nuxt-link class="location" to="#"
+          >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
+        >
+        <div class="times">
+          Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
+          9:00–16:00
+        </div>
+      </div>
+      <div class="opening-two">
+        <nuxt-link class="location" to="#"
+          >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
+        >
+        <div class="times">
+          Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
+          9:00–16:00
+        </div>
+      </div>
+      <div class="social-container"></div>
+    </div>
   </footer>
 </template>
 
@@ -41,7 +61,45 @@ export default defineComponent({
 
 
 <style lang="scss" scoped>
+@import '~assets/styles/config';
+
 .footer-container {
   display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  max-width: 1920px;
+  padding-left: 90px;
+  padding-right: 90px;
+  width: 90vw;
+}
+.newsletter-container {
+  padding-right: 50px;
+  width: 33%;
+}
+.menu-container {
+  padding-left: 50px;
+  width: 33%;
+}
+.opening-container {
+  padding-left: 50px;
+  width: 33%;
+}
+.list-link {
+  color: $orange;
+  line-height: 2.5;
+}
+.list-link:hover {
+  opacity: 0.6;
+}
+.location {
+  color: $orange;
+}
+.location:hover {
+  opacity: 0.6;
+}
+.opening-one {
+  padding-bottom: 50px;
 }
 </style>
