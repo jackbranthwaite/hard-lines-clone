7d531730c1fa5bcb9b98b50eecc3c8c55a4b5e69
Jack <jack.branthwaite@gmail.com>
2022-01-16 01:50:24 +1300

    Progress on footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,40 +1,48 @@
 <template>
-  <footer class="footer-container">
-    <div class="newsletter-container">
-      <h2 class="subsrcibe">SIGN UP TO OUR WEEKLY NEWSLETTER</h2>
-      <p class="sub-subscribe">
-        Get the latest updates, subscriber only deals and really really really
-        good stuff!
-      </p>
-    </div>
-    <div class="menu-container">
-      <ul v-for="(link, i) in bottomMenuItems" :key="i" class="menu-list">
-        <li class="list-item">
-          <nuxt-link to="#" class="list-link">{{ link }}</nuxt-link>
-        </li>
-      </ul>
-      <div class="living-wage-container"></div>
-    </div>
-    <div class="opening-container">
-      <div class="opening-one">
-        <nuxt-link class="location" to="#"
-          >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
-        >
-        <div class="times">
-          Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
-          9:00–16:00
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="newsletter-container">
+        <h2 class="subsrcibe">SIGN UP TO OUR WEEKLY NEWSLETTER</h2>
+        <p class="sub-subscribe">
+          Get the latest updates, subscriber only deals and really really really
+          good stuff!
+        </p>
+      </div>
+      <div class="menu-container">
+        <ul v-for="(link, i) in bottomMenuItems" :key="i" class="menu-list">
+          <li class="list-item">
+            <nuxt-link to="#" class="list-link">{{ link }}</nuxt-link>
+          </li>
+        </ul>
+        <div class="living-wage-container"></div>
+      </div>
+      <div class="opening-container">
+        <div class="opening-one">
+          <nuxt-link class="location" to="#"
+            >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
+          >
+          <div class="times">
+            Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
+            9:00–16:00
+          </div>
+        </div>
+        <div class="opening-two">
+          <nuxt-link class="location" to="#"
+            >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
+          >
+          <div class="times">
+            Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
+            9:00–16:00
+          </div>
+        </div>
+        <div class="social-container"></div>
+        <div class="copyright-container">
+          <div class="copyright-text">
+            Hard Lines Coffee is proud to be a fully accredited living wage
+            employer.<br />Copyright © 2021 Hard Lines Coffee | Outpost ltd.
+          </div>
         </div>
       </div>
-      <div class="opening-two">
-        <nuxt-link class="location" to="#"
-          >Hard Lines Cafe, St Canna Court CF5 1GX</nuxt-link
-        >
-        <div class="times">
-          Mon - Fri | 8:00–17:00<br />Saturday | 9:00–17:00<br />Sunday |
-          9:00–16:00
-        </div>
-      </div>
-      <div class="social-container"></div>
     </div>
   </footer>
 </template>
@@ -62,7 +70,9 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @import '~assets/styles/config';
-
+.footer {
+  background-color: $off--white;
+}
 .footer-container {
   display: flex;
   justify-content: space-between;
@@ -70,8 +80,10 @@ export default defineComponent({
   margin-right: auto;
   margin-top: 50px;
   max-width: 1920px;
+  padding-bottom: 50px;
   padding-left: 90px;
   padding-right: 90px;
+  padding-top: 30px;
   width: 90vw;
 }
 .newsletter-container {
@@ -100,6 +112,9 @@ export default defineComponent({
   opacity: 0.6;
 }
 .opening-one {
+  padding-bottom: 50px;
+}
+.opening-two {
   padding-bottom: 50px;
 }
 </style>
