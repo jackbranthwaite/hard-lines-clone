83eba15ac90e2cf7054c8276efcb676a221f531a
Jack <jack.branthwaite@gmail.com>
2022-02-14 22:20:24 +1300

    Somewhat functioning breadcrumb

--- src/components/breadcrumbs/Breadcrumbs.vue
+++ src/components/breadcrumbs/Breadcrumbs.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="breadcrumbs_container" v-if="params">
-    <ul></ul>
+  <div class="breadcrumbs_container" v-if="breakApart">
+    <ul class="breadcrumbs_list" v-for="(crumb, i) in breakApart" :key="i">
+      <li>
+        <nuxt-link class="breadcrumb" :to="`/${crumb}`"
+          >{{ crumb }}....
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import {
-  computed,
-  defineComponent,
-  useRoute,
-  useRouter
-} from '@nuxtjs/composition-api'
-import { ref } from '@vue/composition-api'
+import { defineComponent, useRoute, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
     const route = useRoute()
     const params = ref(null)
+    const crumbs = ref([])
+    const fullPath = route?.value.fullPath
 
-    const crumbs = computed(() => {
-      const fullPath = route.value?.fullPath
-      params.value = fullPath.startsWith('/')
-        ? fullPath.substring(1).split('/')
-        : fullPath.split('/')
-
-      return crumbs
-    })
+    const breakApart = fullPath.split('/')
 
     return {
-      params
+      params,
+      crumbs,
+      breakApart
     }
   }
 })
 </script>
+<style lang="scss" scoped>
+.breadcrumbs_container {
+  display: flex;
+}
+.breadcrumbs_list {
+  list-style: none;
+}
+.breadcrumb {
+  text-decoration: none;
+}
+</style>
